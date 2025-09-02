abdc744545f0cff0847ac2e8e59440ccb0ab98b6
NaseeraVP <naseeravp38@gmail.com>
2025-09-02 15:44:46 +0530

    fear:Replace Opportunity button to Enquiry button in Lead

--- stems/stems/custom_scripts/lead/lead.js
+++ stems/stems/custom_scripts/lead/lead.js
@@ -1,24 +1,20 @@
 frappe.ui.form.on("Lead", {
 	refresh: function(frm) {
-		// Remove default Opportunity button
 		frm.remove_custom_button("Opportunity", "Create");
-
 		frm.page.remove_inner_button('Opportunity', 'Create');
 
-
-		// Add Enquiry button
 		frm.add_custom_button(__("Enquiry"), function() {
 			frappe.call({
-				method: "stems.stems.doctype.lead.lead.create_enquiry",
+				method: "stems.stems.custom_scripts.lead.lead.create_enquiry",
 				args: {
 					lead: frm.doc.name
 				},
 				callback: function(r) {
 					if (r.message) {
-						frappe.set_route("Form", "Enquiry", r.message);
+						frappe.set_route("Form", "Opportunity", r.message);
 					}
 				}
 			});
-		}, __("Create")); // group it under Create menu
+		}, __("Create"));
 	}
 });
