882295c7abe3259b612f1163cd3f3e56bd898dda
ZakkiyathLulu <zakkiyathlulu@gmail.com>
2025-09-04 14:00:31 +0530

    feat:Removed Add to Prospect action from lead

--- stems/stems/custom_scripts/lead/lead.js
+++ stems/stems/custom_scripts/lead/lead.js
@@ -1,25 +1,33 @@
 frappe.ui.form.on("Lead", {
-	refresh: function(frm) {
-		setTimeout(() => {
-			frm.remove_custom_button("Opportunity", "Create");
-			frm.remove_custom_button("Prospect", "Create");
+    refresh: function(frm) {
+        setTimeout(() => {
+            // Remove Create buttons
+            frm.remove_custom_button("Opportunity", "Create");
+            frm.remove_custom_button("Prospect", "Create");
+            if (frm.page && frm.page.remove_inner_button) {
+                frm.page.remove_inner_button("Opportunity", "Create");
+                frm.page.remove_inner_button("Prospect", "Create");
+            }
 
-			if (frm.page && frm.page.remove_inner_button) {
-				frm.page.remove_inner_button("Opportunity", "Create");
-			}
+            // Remove Action button
+            frm.remove_custom_button("Add to Prospect", "Action");
+            if (frm.page && frm.page.remove_inner_button) {
+                frm.page.remove_inner_button("Add to Prospect", "Action");
+            }
 
-		if (!frm.is_new()) {
-			frm.add_custom_button(__('Enquiry'), function() {
-				frappe.model.open_mapped_doc({
-					method: "stems.stems.custom_scripts.lead.lead.make_enquiry",
-					source_name: frm.doc.name
-				});
-			}, __("Create"));
-		}
-		}, 10); 
-	},
-	setup: function(frm) {
-		set_sales_person_query(frm);
+            // Add Enquiry button under Create group
+            if (!frm.is_new()) {
+                frm.add_custom_button(__('Enquiry'), function() {
+                    frappe.model.open_mapped_doc({
+                        method: "stems.stems.custom_scripts.lead.lead.make_enquiry",
+                        source_name: frm.doc.name
+                    });
+                }, __("Create"));
+            }
+        }, 100);
+    },
+    setup: function(frm) {
+        set_sales_person_query(frm);
     }
 });
 
