7e54cb961dbe6d3f05ae4c4f292395a788a8b57a
ZakkiyathLulu <zakkiyathlulu@gmail.com>
2025-09-04 14:48:29 +0530

    refact:Optimised refresh event by creating a function to edit custom buttons

--- stems/stems/custom_scripts/lead/lead.js
+++ stems/stems/custom_scripts/lead/lead.js
@@ -1,29 +1,7 @@
 frappe.ui.form.on("Lead", {
     refresh: function(frm) {
         setTimeout(() => {
-            // Remove Create buttons
-            frm.remove_custom_button("Opportunity", "Create");
-            frm.remove_custom_button("Prospect", "Create");
-            if (frm.page && frm.page.remove_inner_button) {
-                frm.page.remove_inner_button("Opportunity", "Create");
-                frm.page.remove_inner_button("Prospect", "Create");
-            }
-
-            // Remove Action button
-            frm.remove_custom_button("Add to Prospect", "Action");
-            if (frm.page && frm.page.remove_inner_button) {
-                frm.page.remove_inner_button("Add to Prospect", "Action");
-            }
-
-            // Add Enquiry button under Create group
-            if (!frm.is_new()) {
-                frm.add_custom_button(__('Enquiry'), function() {
-                    frappe.model.open_mapped_doc({
-                        method: "stems.stems.custom_scripts.lead.lead.make_enquiry",
-                        source_name: frm.doc.name
-                    });
-                }, __("Create"));
-            }
+            customize_lead_buttons(frm);
         }, 100);
     },
     setup: function(frm) {
@@ -41,3 +19,32 @@ function set_sales_person_query(frm) {
         };
     });
 }
+
+/*
+ * Function to Add and remove custom buttons on Lead form
+ */
+function customize_lead_buttons(frm) {
+    frm.remove_custom_button("Opportunity", "Create");
+    frm.remove_custom_button("Prospect", "Create");
+    if (frm.page && frm.page.remove_inner_button) {
+        frm.page.remove_inner_button("Opportunity", "Create");
+        frm.page.remove_inner_button("Prospect", "Create");
+    }
+
+    // Remove Action buttons
+    frm.remove_custom_button("Add to Prospect", "Action");
+    if (frm.page && frm.page.remove_inner_button) {
+        frm.page.remove_inner_button("Add to Prospect", "Action");
+    }
+
+    // Add Enquiry button under Create group
+    if (!frm.is_new()) {
+        frm.add_custom_button(__('Enquiry'), function() {
+            frappe.model.open_mapped_doc({
+                method: "stems.stems.custom_scripts.lead.lead.make_enquiry",
+                source_name: frm.doc.name
+            });
+        }, __("Create"));
+    }
+}
+
