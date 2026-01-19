2dfe975b96c0fa4ded2646aafcac5f18fff7b374
anjusha <anjushasuresh85@gmail.com>
2026-01-19 12:19:08 +0530

    refactor(Delivery Note): code cleanup

--- stems/stems/custom_scripts/delivery_note/delivery_note.js
+++ stems/stems/custom_scripts/delivery_note/delivery_note.js
@@ -1,16 +1,22 @@
 frappe.ui.form.on('Delivery Note', {
 	project (frm) {
-		if (frm.doc.project) {
-			frappe.call({
-				method: 'stems.stems.custom_scripts.delivery_note.delivery_note.get_default_project_warehouse',
-				callback: function (r) {
-					if (r.message) {
-						frm.set_value('set_warehouse', r.message);
-					}
-				}
-			});
-		} else {
-			frm.set_value('set_warehouse', '');
-		}
+		set_source_warehouse(frm);
 	}
 });
+
+/**
+ * Sets the Delivery Note's warehouse from STEMS Settings if a project is selected,
+ * or clears it if no project is linked.
+ */
+function set_source_warehouse(frm) {
+	if (frm.doc.project) {
+		frappe.db.get_single_value('STEMS Settings', 'default_project_warehouse')
+			.then(warehouse => {
+				if (warehouse) {
+					frm.set_value('set_warehouse', warehouse);
+				}
+			});
+	} else {
+		frm.set_value('set_warehouse', '');
+	}
+}
--- stems/stems/custom_scripts/delivery_note/delivery_note.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-@frappe.whitelist()
-def get_default_project_warehouse():
-	"""Return the default project warehouse from STEMS Settings."""
-	return frappe.db.get_single_value(
-		"STEMS Settings",
-		"default_project_warehouse"
-	)
